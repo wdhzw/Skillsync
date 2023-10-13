5254b9b8ee7a865f2445acbdc76c752b70902cd3
Huang Zhiwen <zhiwen.huang@u.nus.edu>
2023-10-13 12:35:45 +0000

    Enable message sending and search bar interaction

--- skillsync/src/pages/Chats.js
+++ skillsync/src/pages/Chats.js
@@ -5,24 +5,62 @@ const conversations = [
     {id: 1, name: "John Doe", profileImage: "/images/john-doe.png"},
     {id: 2, name: "Jane Smith", profileImage: "/images/jane-smith.png"},
 ];
+const sampleMessages = {
+    1: [ // Messages for conversation with John Doe (id: 1)
+        {
+            senderId: 1,
+            timestamp: new Date('2023-10-13T12:00:00'),
+            text: "Hi there! I noticed you're skilled in React development. Could you help me out with some concepts?"
+        },
+        {
+            senderId: 2, // Assuming 2 is your user ID
+            timestamp: new Date('2023-10-13T12:05:00'),
+            text: "Of course, John! Which concepts are you struggling with?"
+        },
+        {
+            senderId: 1,
+            timestamp: new Date('2023-10-13T12:10:00'),
+            text: "I'm having trouble understanding hooks, especially useEffect. Can we schedule a call?"
+        }
+    ],
+    2: [ // Messages for conversation with Jane Smith (id: 2)
+        {
+            senderId: 2,
+            timestamp: new Date('2023-10-14T09:00:00'),
+            text: "Hey Jane! I saw you're proficient in Graphic Design. I'm trying to learn the basics, got any tips?"
+        },
+        {
+            senderId: 2,
+            timestamp: new Date('2023-10-14T09:20:00'),
+            text: "Sure! Start with the basic tools and get hands-on practice. Would you like some resources?"
+        }
+    ]
+};
 
 const Chats = () => {
     const [selectedChat, setSelectedChat] = useState(conversations[0].id);
-    const [messages, setMessages] = useState({});
+    const [messages, setMessages] = useState(sampleMessages);
+
     const [currentMessage, setCurrentMessage] = useState("");
     const [searchTerm, setSearchTerm] = useState("");
 
     const handleSendMessage = () => {
         if (!selectedChat) return;
-
+    
+        const newMessage = {
+            senderId: 2, // or whatever the current user's ID is
+            timestamp: new Date(),
+            text: currentMessage
+        };
+    
         setMessages(prevMessages => ({
             ...prevMessages,
             [selectedChat]: [
                 ...(prevMessages[selectedChat] || []),
-                currentMessage
+                newMessage
             ]
         }));
-
+    
         setCurrentMessage("");
     };
 
@@ -60,11 +98,13 @@ const Chats = () => {
                 </div>
                 <div className="chat-body">
                     {(messages[selectedChat] || []).map((message, idx) => (
-                        <div key={idx} className="message user">
-                            {message}
+                        <div key={idx} className={`message ${message.senderId === 2 ? 'self' : 'user'}`}>
+                            {message.text}
                         </div>
                     ))}
                 </div>
+
+
                 <div className="chat-input">
                     <input 
                         type="text" 
