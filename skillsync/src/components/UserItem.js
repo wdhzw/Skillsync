51061f86f3598abb3c41e33a26add466223642d5
Suresh <suresh@u.nus.edu>
2023-12-01 16:18:41 +0800

    User Items display skill name instead of skill id

--- skillsync/src/components/UserItem.js
+++ skillsync/src/components/UserItem.js
@@ -1,43 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'; 
 import './UserItem.css';
 import { Link } from 'react-router-dom';
+import graphQLFetch from '../pages/api.js'; 
 
 function UserItem({ user }) {
   const numberOfSkills = user.profile.skills.length;
+  const [skills, setSkills] = useState([]);
 
-    return (
-                
-      <div>
+  useEffect(() => {
+    const fetchSkills = async () => {
+      const skillsWithDetails = await Promise.all(
+        user.profile.skills.map(async (skillItem) => {
+          const query = `query {
+            getSkill(id: "${skillItem.skill_id}") {
+              id
+              name
+            }
+          }`;
+          const result = await graphQLFetch(query);
+          return result.getSkill;
+        })
+      );
+      setSkills(skillsWithDetails);
+    };
+
+    fetchSkills();
+  }, [user.profile.skills]);
+
+  return (
+    <div>
       <Link to="/ViewProfile">
-
-      <div className="user-item">
-        <img src={user.profile.avatar} width="100" height="50"/> 
-        <h2>{user.username}</h2>
-        <div className="location-bubble">{user.profile.location}</div>
-        <p><h3>{numberOfSkills}</h3></p>
-        <p>Skills to Share</p>
-        <div>
-        {user.profile.skills.map((skillItem, index) => (
-          <div key={index} className="skill-box">
-            {skillItem.skill_id}
+        <div className="user-item">
+          <img src={user.profile.avatar} width="100" height="50" /> 
+          <h2>{user.username}</h2>
+          <div className="location-bubble">{user.profile.location}</div>
+          <p><h3>{numberOfSkills}</h3></p>
+          <p>Skills to Share</p>
+          <div>
+            {skills.map((skill, index) => (
+              <div key={index} className="skill-box">
+                {skill.name}
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-
-        {/* <div>
-          {user.skills.map((skill) => (
-            <div className="skill-box">
-              {skill}
-            </div>
-          ))}
-        </div> */}
-        
-
-      </div>
+        </div>
       </Link>
-      </div>
-    );
-  }
-  
+    </div>
+  );
+}
+
+
   export default UserItem;
   
