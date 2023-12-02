c7a4a5ee6ee2c4c6af3f9970ae1c6112b10802f1
Suresh <suresh@u.nus.edu>
2023-12-02 16:52:14 +0800

    Added sorting, filtering and searching logic for users

--- skillsync/src/pages/UserList.js
+++ skillsync/src/pages/UserList.js
@@ -10,11 +10,21 @@ export default function UserList() {
     const [searchTerm, setSearchTerm] = useState('');
     const [selectedLocation, setSelectedLocation] = useState('');
     const [selectedSkill, setSelectedSkill] = useState('');
-    
+    const [skillsList, setSkillsList] = useState([]);
+    const [sortOrder, setSortOrder] = useState('desc'); 
+    const [skillNameToId, setSkillNameToId] = useState({});
+
     const [editingUser, setEditingUser] = useState(null);
     const [isEditModalOpen, setIsEditModalOpen] = useState(false);
     const userRole = localStorage.getItem('userRole');
-
+    const districts = [
+        "Ang Mo Kio", "Bedok", "Bishan", "Bukit Batok", "Bukit Merah",
+        "Choa Chu Kang", "Clementi", "Geylang", "Hougang", "Jurong East",
+        "Jurong West", "Kallang", "Marine Parade", "Orchard", "Pasir Ris",
+        "Punggol", "Queenstown", "Sembawang", "Sengkang", "Serangoon",
+        "Tampines", "Toa Payoh", "Woodlands", "Yishun", "Jurong",
+        "Bukit Timah", "Novena", "Tanglin", "Rochor"];
+    
     function getUsers() {
         const getAllUsersQuery = `
           query getAllUsers {
@@ -48,6 +58,40 @@ export default function UserList() {
           });
       }
 
+      skillsList.forEach(skill => {
+          skillNameToId[skill.name] = skill.id;
+      });
+      
+      useEffect(() => {
+        const fetchSkills = async () => {
+            const query = `query {
+                getAllSkills {
+                    id
+                    name
+                    description
+                    pic
+                }
+            }`;
+    
+            const data = await graphQLFetch(query);
+            if (data) {
+                const skillsMapping = {};
+                data.getAllSkills.forEach(skill => {
+                    skillsMapping[skill.name] = parseInt(skill.id, 10); 
+                });
+                setSkillsList(data.getAllSkills);
+                setSkillNameToId(skillsMapping);             }
+    
+
+        };
+    
+        fetchSkills();
+    }, []);
+
+
+
+    
+
     function handleEditClick(user) {
         setEditingUser(user);
         setIsEditModalOpen(true);
@@ -80,13 +124,31 @@ export default function UserList() {
 
     const handleSkillChange = (e) => {
         setSelectedSkill(e.target.value);
+        console.log('Selected skill:', e.target.value); 
     }
 
-    // const displayedUsers = users.filter(user =>
-    //     user.name.toLowerCase().includes(searchTerm.toLowerCase()) &&
-    //     (selectedLocation === '' || user.location === selectedLocation) &&
-    //     (selectedSkill === '' || user.skills.includes(selectedSkill))
-    // );
+    console.log('Before Filtering - skillNameToId', skillNameToId);
+    console.log('Before Filtering - selectedSkill', selectedSkill);
+    console.log('Before Filtering - selectedSkillId', skillNameToId[selectedSkill]);
+    
+    const displayedUsers = users
+    .filter(user => {
+        const selectedSkillId = skillNameToId[selectedSkill];
+        console.log('Filtering with selectedSkillId:', selectedSkillId); // Debugging
+        console.log('User skills:', user.profile.skills); // Debugging
+    
+        return user.username.toLowerCase().includes(searchTerm.toLowerCase()) &&
+        (selectedLocation === '' || user.profile.location === selectedLocation) &&
+        (selectedSkill === '' || user.profile.skills.some(skill => parseInt(skill.skill_id,10) === parseInt(selectedSkillId,10)));
+         })
+    .sort((a, b) => {
+        if (sortOrder === 'desc') {
+            return b.profile.skills.length - a.profile.skills.length;
+        } else {
+            return a.profile.skills.length - b.profile.skills.length;
+        }
+    });
+
 
     useEffect(() => {
         getUsers();
@@ -105,44 +167,42 @@ export default function UserList() {
                 <div className="filters">
                 Filter By:
                 <select value={selectedLocation} onChange={handleLocationChange}>
-                    <option value="">Location</option>
-                    <option value="Hougang">Hougang</option>
-                    <option value="Paya Lebar">Paya Lebar</option>
-                    <option value="Thompson">Thompson</option>
-
+                <option value="">Location</option>
+                {districts.map(district => (
+                <option key={district} value={district}>{district}</option>
+                ))}
                 </select>
-                <select value={selectedSkill} onChange={handleSkillChange}> 
-                    <option value="">Skills to Share</option>
-                    <option value="programming">Programming</option>
-                    <option value="javascript">Javascript</option>
-                    <option value="java">Java</option>
-                    <option value="c++">C++</option>
 
+                <select value={selectedSkill} onChange={handleSkillChange}>
+                <option value="">Skills to Share</option>
+                {skillsList.map(skill => (
+                <option key={skill.id} value={skill.name}>{skill.name}</option>
+                ))}
                 </select>
+
 
                 </div>
                 <div className="sort">
                 Sort By:
-                <select>
-                    <option value="SkillstoShare">No of Skills to Share</option>
-                </select>
+                    <select onChange={(e) => setSortOrder(e.target.value)}>
+                    <option value="desc">No of Skills Desc</option>
+                    <option value="asc">No of Skills Asc</option>
+                    </select>
                 </div>
             </div>
 
             <div className="user-grid">
-                {
-                    users.map(user => (
-                        <div className="user-grid-item" key={user.id}>
-                            <UserItem user={user} />
-                            {userRole === 'admin' && (
-                                <>
-                                    <button className="edit" onClick={() => handleEditClick(user)}>Edit</button>
-                                    <button onClick={() => handleDelete(user.id)}>Delete</button>
-                                </>
-                            )}
-                        </div>
-                    ))
-                }
+            {displayedUsers.map(user => (
+            <div className="user-grid-item" key={user.id}>
+            <UserItem user={user} />
+            {userRole === 'admin' && (
+            <>
+                <button className="edit" onClick={() => handleEditClick(user)}>Edit</button>
+                <button onClick={() => handleDelete(user.id)}>Delete</button>
+            </>
+            )}
+            </div>
+            ))}
             </div>
 
             {isEditModalOpen && (
